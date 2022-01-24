04dcc31eee7764e7129b30560770bba81a91b2f9
Ser_Yo_Ja <seroja.92@mail.ru>
2022-01-24 22:11:29 +0300

    Initial commit

---
+++ src/App.kt
@@ -0,0 +1,14 @@
+fun main(args: Array<String>){
+    println()
+    println("Pow!")
+
+    comands();
+}
+
+fun comands() {
+    var x = 3
+    val name = "Cormoran"
+    x = x * 10
+    print("x is $x")
+    //Some comment
+}
