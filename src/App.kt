207c20f55ede667d172122e0709119bfbdab4941
Ser_Yo_Ja <seroja.92@mail.ru>
2022-01-25 14:22:24 +0300

    первый код на Kotlin

--- src/App.kt
+++ src/App.kt
@@ -1,14 +1,27 @@
-fun main(args: Array<String>){
-    println()
-    println("Pow!")
+fun main(args: Array<String>) {
+    var x = 1
+    println("Before the loop x = $x")
+    while (x < 4) {
+        println("In the loop x = $x")
+        x = x + 1
+    }
+    println("After the loop x = $x")
 
-    comands();
+    println("")
+
+    val a = 2
+    val b = 4
+    println(if (a > b) "a greater than b" else "a isn't greater than b")
+    println("This lines runs no matter what")
+
+    println("")
+
+    var z = 1
+    while (z < 3) {
+        print(if (z == 1) "Yab" else "Dab")
+        print("ba")
+        z = z + 1
+    }
+    if (z == 3) println("Do")
 }
 
-fun comands() {
-    var x = 3
-    val name = "Cormoran"
-    x = x * 10
-    print("x is $x")
-    //Some comment
-}
